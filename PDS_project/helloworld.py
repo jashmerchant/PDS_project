857c0677d1383bf52659351178ca9d0fcabca525
Jash Merchant <merchantjash@gmail.com>
2022-08-09 13:10:08 -0400

    Introduce templates

--- PDS_project/helloworld.py
+++ PDS_project/helloworld.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Hello, World!</h1>"
+    return render_template("home.html")
 
 @app.route("/login")
 def login():
-    return "<h1>Login Page</h1>"
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
