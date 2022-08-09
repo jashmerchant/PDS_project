144692a3180de2035f2ed599ca0a8823c7a0acb5
Jash Merchant <merchantjash@gmail.com>
2022-08-09 13:05:09 -0400

    set debug as true

--- PDS_project/helloworld.py
+++ PDS_project/helloworld.py
@@ -3,8 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+@app.route("/home")
+def home():
     return "<h1>Hello, World!</h1>"
 
+@app.route("/login")
+def login():
+    return "<h1>Login Page</h1>"
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
