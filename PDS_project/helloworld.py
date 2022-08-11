8dc6aeecf259a01ed5036a7e06b7a55d65e74e7d
Jash Merchant <merchantjash@gmail.com>
2022-08-11 13:11:58 -0400

    Make UI consistent

--- PDS_project/helloworld.py
+++ PDS_project/helloworld.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
