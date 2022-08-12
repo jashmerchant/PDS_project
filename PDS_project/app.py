c9d527caa03611e2d9843930853e70ab5505af71
Jash Merchant <merchantjash@gmail.com>
2022-08-12 14:03:50 -0400

    Create signup page

--- PDS_project/app.py
+++ PDS_project/app.py
@@ -11,5 +11,9 @@ def home():
 def login():
     return render_template("login.html")
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    return render_template("register.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
